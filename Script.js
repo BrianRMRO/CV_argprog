1714b09e2782891a090f5c51a0b541e4d632bcd3
Brian <ezeromero15@hotmail.com>
2023-01-19 22:36:25 -0300

    modificación del fondo y mejoras con JS

--- Script.js
+++ Script.js
@@ -1,3 +1,9 @@
 function popup() {
-    alert("Gracias por comunicarte, en brebe te responderé")
+    window.alert("Gracias por comunicarte, en brebe te responderé");
 }
+
+const btnMenu = document.querySelector("#btnMenu");
+const menu = document.querySelector ("#menu");
+btnMenu.addEventListener("click",function(){
+    menu.classList.toggle("mostrar");
+});
