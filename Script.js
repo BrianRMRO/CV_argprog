8079c98bee3db4df119c287032f55e13ad8b53ca
Brian <ezeromero15@hotmail.com>
2023-01-27 16:37:56 -0300

    Cambios en botones y funciones con JS

--- Script.js
+++ Script.js
@@ -7,3 +7,26 @@ const menu = document.querySelector ("#menu");
 btnMenu.addEventListener("click",function(){
     menu.classList.toggle("mostrar");
 });
+
+
+function escribirEnPantalla(texto){
+    let arr=texto.split("");
+    i=0;
+   let intervalo= setInterval(function(){
+      if(i==arr.length-1){ 
+        document.getElementById("cabecera").innerHTML+=arr[i];
+        clearInterval(intervalo);} 
+      else {
+      if(arr[i]==" "){
+       document.getElementById("cabecera").innerHTML+=arr[i];
+       document.getElementById("cabecera").innerHTML+=arr[i+1];
+        i+=2;
+      }else{
+      document.getElementById("cabecera").innerHTML+=arr[i];
+      i++;
+        }
+      }
+    },150); 
+  }
+  
+  escribirEnPantalla("Pablo Vasquez, desarrollador web");
